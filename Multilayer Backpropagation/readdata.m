560363e69911fc3ea7d1269df58e617cae8e9f7e
Muyun Liu <35407719+tuziluobo@users.noreply.github.com>
2018-01-29 10:01:48 -0800

    Update readdata.m

--- Multilayer Backpropagation/readdata.m
+++ Multilayer Backpropagation/readdata.m
@@ -4,14 +4,20 @@ testset=loadMNISTImages('t10k-images.idx3-ubyte');
 testlabel=loadMNISTLabels('t10k-labels.idx1-ubyte');
 trainset=trainset*255/127.5-1;
 testset=testset*255/127.5-1;
-holdset=trainset(:,50001:60000);
+r=randperm(size(trainset, 2)); %shuffle
+trainset=trainset(:,r);
 trainlabel=trainlabel';
+trainlabel=trainlabel(:,r);
+holdset=trainset(:,50001:60000);
 holdlabel=trainlabel(:,50001:60000);
 trainset=trainset(:,1:50000);
 trainlabel=trainlabel(:,1:50000);
-w1=normrnd(0,1,28*28,64);
-w2=normrnd(0,1,64,10);
+w1=normrnd(0,1/28/28,28*28,64); %initialize
+w2=normrnd(0,1/64,64,10);
 [error_te]=neuralnetwork(trainset, trainlabel, holdset, holdlabel, testset, testlabel, w1,w2);
+
+
+
 
 
 
